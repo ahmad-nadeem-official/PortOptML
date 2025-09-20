21774c573cb694e4374c86d7992a23d57654ff62
ahmad-nadeem-official <ahmadnadeem701065@gmail.com>
2025-09-20 17:15:17 +0500

    done

--- main/pages/news.py
+++ main/pages/news.py
@@ -1,11 +1,15 @@
 import feedparser
 import streamlit as st
 
+
+st.set_page_config(layout="wide")
+st.title("Latest News about your favorite stocks")
+
 ticker = "AAPL"
 url = f"https://finance.yahoo.com/rss/headline?s={ticker}"
 feed = feedparser.parse(url)
 
-for entry in feed.entries[:5]:
+for entry in feed.entries[:10]:
     title = entry.title
     link = entry.link
     

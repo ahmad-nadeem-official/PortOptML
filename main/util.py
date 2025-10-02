3d23934bf1a86d189f699ca1d7ad5ff06f3e6154
ahmad-nadeem-official <ahmadnadeem701065@gmail.com>
2025-10-02 13:55:20 +0500

    done

--- main/util.py
+++ main/util.py
@@ -1,5 +1,7 @@
-from PIL import Image
+import cv2
 
-img = Image.open("Tradmincer.png")
-bigger = img.resize((1000, 1000), Image.LANCZOS)  # upscale to 1000x1000
-bigger.save("Tradmincer_big.png")
+image = cv2.imread("Tradmincer_big.png")
+resized_image = cv2.resize(image, (2024, 2024), interpolation=cv2.INTER_LANCZOS4)
+cv2.imwrite("main/Tradmincer_big2.png", resized_image)
+print("Image resized and saved as main/Tradmincer_big2.png")
+# cv2.INTER_LANCZOS4 is a good interpolation method for enlarging images.
